8be0c6b2307ef7e5c3198ded3aac26c6aa3656b1
benyezzzaa <fatmabenyezza17@gmail.com>
2023-09-24 09:04:20 +0100

    added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './BasicExample';
+import Cards from './Cards';
+import AlertDismissibleExample from './AlertDismissibleExample';
+
+function App() {
+  return (
+    <>
+    <BasicExample></BasicExample>
+    <Cards></Cards>
+    <Cards></Cards>
+    <Cards></Cards>
+    <AlertDismissibleExample></AlertDismissibleExample>
+    </>
+  );
+}
+
+export default App;
